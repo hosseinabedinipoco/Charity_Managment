79cde80afb60d72ebaf3347911a6cf17419c3cca
Hossein <hosseinabedinipoco@gmail.com>
2024-07-29 18:15:42 +0330

    create my custom usre in acounnt models

--- Charity_Managment/Charity_Managment/settings.py
+++ Charity_Managment/Charity_Managment/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+AUTH_USER_MODEL = 'account.User'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
+    'Charity.apps.CharityConfig'
 ]
 
 MIDDLEWARE = [
--- Charity_Managment/account/models.py
+++ Charity_Managment/account/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+
+    GENDER = (
+        ('f', 'famale'),
+        ('m', 'male')
+    )
+
+    address = models.TextField(null=True)
+    age = models.PositiveSmallIntegerField(null=True)
+    description = models.TextField(null=True)
+    email = models.EmailField(null=True)
+    date_joined = models.DateTimeField()
+    first_name = models.CharField(max_length=20, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    last_login = models.DateTimeField(null=True)
+    last_name = models.CharField(max_length=20, null=True)
+    password = models.CharField(max_length=20)
+    phone = models.CharField(max_length=11, null=True)
+    username = models.CharField(max_length=20, unique=True)
