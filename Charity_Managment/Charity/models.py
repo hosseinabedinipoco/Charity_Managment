d6a188e0b45d5c72ca0958c7938411d6c09fd5d4
Hossein <hosseinabedinipoco@gmail.com>
2024-07-29 18:42:33 +0330

    add benefactor model

--- Charity_Managment/Charity/models.py
+++ Charity_Managment/Charity/models.py
@@ -6,3 +6,15 @@ class Charity(models.Model):
     name = models.CharField(max_length=50)
     reg_number = models.CharField(max_length=10)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Benefactor(models.Model):
+    LEVEL = (
+        (0, "Bigginer"),
+        (1, 'Midlevel'),
+        (2, 'expert')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    experience = models.SmallIntegerField(default=0, choices=LEVEL)
+    free_time_per_week = models.PositiveIntegerField(default=0)
+
+        
