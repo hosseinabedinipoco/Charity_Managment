0c455013cfde49d105e6578b99d1b75bcee7c235
Hossein <hosseinabedinipoco@gmail.com>
2024-07-29 18:37:16 +0330

    add Charity model

--- Charity_Managment/Charity/models.py
+++ Charity_Managment/Charity/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from account.models import User
 # Create your models here.
+
+class Charity(models.Model):
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
