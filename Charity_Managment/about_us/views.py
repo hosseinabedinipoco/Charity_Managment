5dca73ee949a27b1f5bd9b5dc80214090ec8e65d
Hossein <hosseinabedinipoco@gmail.com>
2024-07-30 12:05:39 +0330

    add about_us functionality in views

--- Charity_Managment/about_us/views.py
+++ Charity_Managment/about_us/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from account.models import User
 # Create your views here.
+def about_us(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        return render(request, 'about_us.html', {'users':users})
+    else:
+        return 'mehod not allowed'
